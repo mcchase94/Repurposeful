1e338571631e79f62688736256d3aeb68b58c138
mcchase94 <127261330+mcchase94@users.noreply.github.com>
2023-03-10 11:35:46 -0800

    Update bg_remove.py

--- bg_remove.py
+++ bg_remove.py
@@ -7,9 +7,7 @@ import base64
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
 st.write("## Remove background from your image")
-st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
-)
+st.write("Video background remover coming in Version 2.01, stay tuned!")
 st.sidebar.write("## Upload and download :gear:")
 
 
