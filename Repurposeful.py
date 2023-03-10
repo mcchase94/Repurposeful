3f2ee9a7cd64dddd12b0b68fc19508e0545c4d93
mcchase94 <127261330+mcchase94@users.noreply.github.com>
2023-03-10 08:11:03 -0800

    Add files via upload

---
+++ Repurposeful.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+## Install Libraries
+
+
+# In[2]:
+
+
+import streamlit as st
+from rembg import remove
+from PIL import Image
+import base64
+from io import BytesIO
+
+
+# In[11]:
+
+
+st.set_page_config(layout="wide", page_title="Image Background Remover")
+
+st.write("## Remove Background from your image")
+st.write("3D Background Removal coming soon!")
+st.sidebar.write("## Upload and download :gear:")
+
+
+# Download the fixed image
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
+
+
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("Original Image :camera:")
+    col1.image(image)
+
+    fixed = remove(image)
+    col2.write("Fixed Image :wrench:")
+    col2.image(fixed)
+    st.sidebar.markdown("\n")
+    st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
+
+
+col1, col2 = st.columns(2)
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if my_upload is not None:
+    fix_image(upload=my_upload)
+else:
+    fix_image("./camel.jpg")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
